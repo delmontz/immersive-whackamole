42601613caf2156bb94db2b9c82a7b9249166162
delmont <pazxx219@gmail.com>
2018-05-21 23:16:21 +0900

    moleコンポーネント追加 イマーシブ1 35:42

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,40 @@
 <template>
-   <!-- インジケータ類 -->
-   <div class="whackmole">
-      <div class= "logo">モグラたたき！</div>
-      <button class="game-start" v-on:click="gameStart">ゲーム開始</button>
-      <div class=counter-container>
-         <div class="counter">
-            <h1>whacks:</h1>
-            <h2>{{ score }}</h2>
-         </div>
-         <div class="counter">
-            <h1>得点</h1>
-            <h2>{{ highScore }}</h2>
-         </div>
-         <div class="counter">
-            <h1>残り時間</h1>
-            <h2>{{ timer }}</h2>
-         </div>
+  <!-- インジケータ類 -->
+  <div class="whackmole">
+    <div class= "logo">モグラたたき！</div>
+    <button class="game-start" v-on:click="gameStart">ゲーム開始</button>
+    <div class=counter-container>
+      <div class="counter">
+        <h1>whacks:</h1>
+        <h2>{{ score }}</h2>
       </div>
-      <!-- モグラ表示 -->
-      <div class="mole-container">
-            <div class="mole">{{ moles[0] }}</div>
-            <div class="mole">{{ moles[1] }}</div>
-            <div class="mole">{{ moles[2] }}</div>
-            <div class="mole">{{ moles[3] }}</div>
+      <div class="counter">
+        <h1>得点</h1>
+        <h2>{{ highScore }}</h2>
       </div>
-   </div>
+      <div class="counter">
+        <h1>残り時間</h1>
+        <h2>{{ timer }}</h2>
+      </div>
+    </div>
+    <!-- モグラ表示 -->
+    <Moles />
+  </div>
 </template>
 
 <script>
+import Moles from './components/Moles';
+
 export default {
-   data: () => {
-      return {
-         score: 0,
-         highScore: 15,
-         timer: 20,
-         moles: [false, false, false, false]
-      };
+  name: 'App',
+  components: {'Moles': Moles},
+  data: () => { //お作法 コンポーネントにデータを渡すときは関数にしてreturnしないといけない
+    return {
+      score: 0,
+      highScore: 15,
+      timer: 20,
+      moles: [false, false, false, false]
+    };
    },
    methods: {
       gameStart: () => {
---
+++ src/components/Moles.vue
@@ -0,0 +1,23 @@
+<template>
+   <div class="mole-container">
+      <div class="mole">{{ moles[0] }}</div>
+      <div class="mole">{{ moles[1] }}</div>
+      <div class="mole">{{ moles[2] }}</div>
+      <div class="mole">{{ moles[3] }}</div>
+   </div>
+</template>
+
+<script>
+export default {
+   name: 'Moles',
+   data: () => {
+      return {
+         moles: [false, false, false, false]
+      };
+   }
+}
+</script>
+
+<style>
+
+</style>
